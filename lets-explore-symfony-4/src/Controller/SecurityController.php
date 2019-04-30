97aa487bb8403097889e23f78854a5670324962e
Eleonora Mariscalco <eleonoramariscalco@MacBook-Pro-di-Eleonora.local>
2019-04-30 11:00:16 +0200

    qr code decoder integration

--- lets-explore-symfony-4/src/Controller/SecurityController.php
+++ lets-explore-symfony-4/src/Controller/SecurityController.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace App\Controller;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Zxing\QrReader;
+
 
 class SecurityController extends AbstractController
 {
@@ -19,6 +20,15 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+        $image = __DIR__  .  "/../../public/img/qrcode.png";
+        $qrcode = new QrReader($image);
+        $qrcode->text();
+        dump($qrcode);
+        //legge il qr code correttamente, vedere il result trovare un modo per usarlo per il login e buona pasqua
+
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+
+
 }
